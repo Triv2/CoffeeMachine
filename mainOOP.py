51b08e29a7235b5ac8c6e07e5e802d9438a6b27d
Trivial-lore <triviallore@gmail.com>
2023-06-28 11:28:17 -0500

    setting up OOP version of project

---
+++ mainOOP.py
@@ -0,0 +1,9 @@
+#this is the same script as main.py, but using Object Oriented Programming
+from turtle import Turtle, Screen
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
